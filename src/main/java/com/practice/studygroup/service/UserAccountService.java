1d8b6f7e67b0929fc5f89928a6b19d5f912b1159
waonderboy <crazy7523@gmail.com>
2022-10-27 17:46:44 +0900

    #7 - 이메일 인증 및 회원가입시 로그인 기능 제공
    이메일 인증 및 인증여부를 저장하는 기능을 작성하였다. 엔티티를 직접 건드리기 때문에 트랜잭션 처리를 하였다.
    또한 검증된 이메일로 자동 로그인 기능을 지원하는데 UserDetails, AuthProvider, Filter, SecurityContext 등을 레이어를 무시하고 쓰는 것을 볼 수 있다. 이는 추후에 인증, 인가 기능이 변경되면 분리될 수 있다.

--- src/main/java/com/practice/studygroup/service/UserAccountService.java
+++ src/main/java/com/practice/studygroup/service/UserAccountService.java
@@ -6,7 +6,21 @@ import com.practice.studygroup.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +28,11 @@ public class UserAccountService {
 
     private final UserAccountRepository userAccountRepository;
     private final JavaMailSender javaMailSender;
-    
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
     public void processNewUserAccount(UserAccountDto userAccountdto) {
-        UserAccount newUserAccount = userAccountRepository.save(userAccountdto.toEntity());
+        UserAccount newUserAccount = userAccountRepository.save(userAccountdto.toEntity(passwordEncoder));
         newUserAccount.generateEmailCheckToken();
         sendSignUpConfirmEmail(newUserAccount);
     }
@@ -29,6 +45,31 @@ public class UserAccountService {
                 + "&email=" + userAccount.getEmail());
 
         javaMailSender.send(mailMessage); //TODO : SMTP로 구현필요
+    }
+
+    /**
+     * authentication Filter, Manager, Provider, Service 등 나중에 고도화 시
+     * 시큐리티 빈으로 등록해서 주입후 사용
+     * @param
+     */
+    public UserAccountDto loginAfterSignUp(String email) {
+        UserAccount userAccount = userAccountRepository.findByEmail(email).orElse(null);
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                userAccount.getNickname(),
+                userAccount.getPassword(),
+                Set.of(new SimpleGrantedAuthority("ROLE_USER")));
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContext.setAuthentication(token);
+
+        return UserAccountDto.from(userAccount);
+    }
+
+    @Transactional
+    public boolean isNotCorrectTokenAndSignUp(String email, String token) {
+        UserAccount userAccount = userAccountRepository.findByEmail(email).orElse(null);
+
+        return (userAccount != null && userAccount.isValidToken(token)) ?
+                userAccount.completeSignUp() : false;
     }
 
 }
---
+++ src/test/java/com/practice/studygroup/service/UserAccountServiceTest.java
@@ -0,0 +1,56 @@
+package com.practice.studygroup.service;
+
+import com.practice.studygroup.domain.UserAccount;
+import com.practice.studygroup.dto.UserAccountDto;
+import com.practice.studygroup.repository.UserAccountRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+@DisplayName("서비스 - 회원가입")
+@ExtendWith(MockitoExtension.class)
+class UserAccountServiceTest {
+
+    @InjectMocks
+    private UserAccountService sut;
+    @Mock
+    private PasswordEncoder passwordEncoder;
+    @Mock
+    private JavaMailSender javaMailSender;
+    @Mock
+    private UserAccountRepository userAccountRepository;
+
+    @DisplayName("회원가입 정보가 주어지면, 비밀번호를 인코딩해 디비에 저장 후 메일을 발송한다")
+    @Test
+    void UserAccountDto_SaveEncodedUserAccount_DoNothing() {
+        // Given
+        UserAccountDto dto = createUserAccountDto();
+        UserAccount entity = dto.toEntity(passwordEncoder);
+        given(userAccountRepository.save(entity)).willReturn(entity);
+
+        // When
+        sut.processNewUserAccount(dto);
+
+        // Then
+        assertThat(dto.getPassword()).isNotSameAs(entity.getPassword());
+        then(userAccountRepository).should().save(entity);
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.builder()
+                .email("bilnd@naver.com")
+                .password("adddfas@!#")
+                .nickname("blind13")
+                .build();
+    }
+
+}
