889350cc83babbbf6472ef41434d63cf96c49253
waonderboy <crazy7523@gmail.com>
2022-10-30 14:22:52 +0900

    #4 - RememberMe 토큰 발급 기능 추가
    username serires token으로 구성된 RememberMe 토큰을 발급하는 기능을 추가하였다. 해당 기능을 추가하면 세션이 만료되어도 로그인 상태를 유지할 수 있다
    또한 리멤버미 토큰을 만들기 위해서는 디비에 토큰테이블이 필요한데, ORM으로 테이블 생성을 위해 토큰 엔티티를 만들어 주었다.

--- src/main/java/com/practice/studygroup/config/SecurityConfig.java
+++ src/main/java/com/practice/studygroup/config/SecurityConfig.java
@@ -8,15 +8,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+    private final DataSource dataSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -37,7 +45,17 @@ public class SecurityConfig {
                 .logout()
                     .logoutSuccessUrl("/")
                     .and()
+                .rememberMe().userDetailsService(userDetailsService)
+                    .tokenRepository(tokenRepository())
+                .and()
                 .build();
+    }
+
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+
+        return jdbcTokenRepository;
     }
 
     @Bean
