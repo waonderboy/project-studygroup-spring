b37544f8e56f572a08292c4591733fde4bf28ef6
waonderboy <crazy7523@gmail.com>
2022-11-02 15:04:28 +0900

    #17 - 패스워드 및 닉네임 검증클래스 개발

---
+++ src/main/java/com/practice/studygroup/validator/PasswordFormValidator.java
@@ -0,0 +1,30 @@
+package com.practice.studygroup.validator;
+
+import com.practice.studygroup.dto.request.PasswordForm;
+import com.practice.studygroup.dto.request.SignUpForm;
+import com.practice.studygroup.repository.UserAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class PasswordFormValidator implements Validator {
+
+    private final UserAccountRepository userAccountRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(PasswordForm.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        PasswordForm passwordForm = (PasswordForm) target;
+
+        if (!passwordForm.getNewPassword().equals(passwordForm.getNewPasswordConfirm())) {
+            errors.rejectValue("newPassword", "wrong.value", "입력한 새 패스워드가 일치하지 않습니다.");
+        }
+    }
+}
